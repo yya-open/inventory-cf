dd5f8278d3b70e357668f21a9c2d2e3ce487c2a7
yya-open <1820959455@qq.com>
2026-02-27 13:19:53 +0800

    Update PcRecycle.vue

--- src/views/PcRecycle.vue
+++ src/views/PcRecycle.vue
@@ -23,7 +23,12 @@
       </div>
 
 
-      <el-form-item label="选择电脑" prop="asset_id">
+      <!--
+        修复 Chrome DevTools Issues: Incorrect use of <label for=FORM_ELEMENT>
+        Element Plus 的 el-form-item 会生成带 for 的 label；需要确保能匹配到表单控件的 id。
+      -->
+
+      <el-form-item label="选择电脑" prop="asset_id" label-for="pc-recycle-asset">
         <el-select
           v-model="form.asset_id"
           filterable
@@ -31,6 +36,8 @@
           :remote-method="remoteSearch"
           :loading="assetLoading"
           placeholder="输入序列号/品牌/型号搜索（仅显示已领用）"
+          id="pc-recycle-asset"
+          name="asset_id"
           style="width: 100%"
           @change="onPickAsset"
           clearable
@@ -73,18 +80,20 @@
         <el-descriptions-item label="预计回收">{{ pickedAsset.last_recycle_date || '-' }}</el-descriptions-item>
       </el-descriptions>
 
-      <el-form-item label="回收/归还日期" prop="recycle_date">
+      <el-form-item label="回收/归还日期" prop="recycle_date" label-for="pc-recycle-date">
         <el-date-picker
           v-model="form.recycle_date"
           type="date"
           value-format="YYYY-MM-DD"
           placeholder="选择日期"
+          id="pc-recycle-date"
+          name="recycle_date"
           style="width: 100%"
         />
       </el-form-item>
 
-      <el-form-item label="备注">
-        <el-input v-model="form.remark" type="textarea" :rows="3" />
+      <el-form-item label="备注" label-for="pc-recycle-remark">
+        <el-input v-model="form.remark" type="textarea" :rows="3" input-id="pc-recycle-remark" name="remark" />
       </el-form-item>
 
       <el-form-item>
