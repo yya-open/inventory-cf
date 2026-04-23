80ff10bee7fe02368442e142db5d30e611a03dde
yya-open <1820959455@qq.com>
2026-04-23 11:21:19 +0800

    减轻首屏

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import router from './router';
 import { installElementPlus } from './plugins/element-plus';
 import './styles/app.css';
 import { startBuildVersionWatcher } from './utils/appVersion';
+import { scheduleOnIdle } from './utils/idle';
 
 startBuildVersionWatcher();
+scheduleOnIdle(() => {
+  void import('./plugins/element-plus-extra-styles');
+}, 1400);
 
 const app = createApp(App);
 installElementPlus(app);
